95e14b0d3a19ecda379a56f42d7abf039b2e3a29
sohel5G <contactsohelrana@gmail.com>
2023-08-06 13:37:35 +0600

    slice and splice pracftise

---
+++ array.js
@@ -0,0 +1,10 @@
+let testArray = [ 'one', 'two' ];
+//console.log( Array.isArray(testArray) );
+// console.log( testArray.includes( 'two' ) );
+
+let testArray2 = [ 'three', 'four' ];
+let dueArrayJuraLaga = testArray.concat(testArray2);
+console.log(dueArrayJuraLaga);
+
+
+
---
+++ slice_and_splice.js
@@ -0,0 +1,11 @@
+
+let testArray1 = [ 'One', 'Two', 'Three', 'Four', 'Five', 'six' ];
+
+// let mySlice = testArray1.slice(1, 4);
+// console.log(mySlice)
+
+let mySplice = testArray1.splice(1, 4, 444, 'dddd');
+console.log(mySplice);
+
+console.log( testArray1 );
+
