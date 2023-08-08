23bdde1b691f80177508a37d5590a190de85f8e6
sohel5G <contactsohelrana@gmail.com>
2023-08-08 17:07:03 +0600

    Almost done

---
+++ for_in_of.js
@@ -0,0 +1,22 @@
+
+
+// const person = {fname:"John", lname:"Doe", age:25};
+// for(let x in person){
+//     console.log( x + ' : ' +  person[x] );
+// }
+
+
+let fullName = [200, 400, 'Hello', 'Blue', 20, 99, 'ANything'];
+
+for( let x of fullName ){
+    console.log(x);
+    console.log( fullName[x] );
+}
+
+
+
+
+
+
+
+
